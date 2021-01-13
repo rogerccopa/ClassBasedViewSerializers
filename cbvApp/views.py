24119d33ad26b6c1975bec273f8211f608c326af
Roger Ccopa <rogerccopa@gmail.com>
2021-01-13 14:14:30 -0500

    Mixins based API Implementation

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -6,6 +6,36 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
 
+from rest_framework import generics, mixins
+
+### Mixins based API Implementation ###
+
+class StudentList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request):
+        return self.list(request)
+    
+    def post(self, request):
+        return self.create(request)
+
+class StudentDetail(mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
+
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
+
+'''
+### RAW API Implementation ###
+
 class StudentList(APIView):
 
     def get(self,request):
@@ -45,3 +75,4 @@ class StudentDetail(APIView):
         student = self.get_object(pk)
         student.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+'''
