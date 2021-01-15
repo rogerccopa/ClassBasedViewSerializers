37a6ec0a7d26bebd8bb6797bbcd24ab803264438
Roger Ccopa <rogerccopa@gmail.com>
2021-01-15 12:49:49 -0500

    rest framework class based pagination

--- cbvApp/views.py
+++ cbvApp/views.py
@@ -8,10 +8,15 @@ from django.http import Http404
 
 from rest_framework import generics, mixins
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
+
+class StudentPagination(PageNumberPagination):
+    page_size = 2
 
 class StudentViewSet(viewsets.ModelViewSet):
     queryset = Student.objects.all()
     serializer_class = StudentSerializer
+    pagination_class = StudentPagination
 
 '''
 ### Generics based API Implementation ###
--- cbvSerializers/settings.py
+++ cbvSerializers/settings.py
@@ -43,9 +43,10 @@ INSTALLED_APPS = [
     'cbvApp'
 ]
 
+# This config method affects pagination globally (default)
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2
+    'PAGE_SIZE': 1
 }
 
 MIDDLEWARE = [
